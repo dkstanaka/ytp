7ad42c1e3d1b529e886251df30341dedc6f4433c
v45wrider <v45wrider@outlook.jp>
2017-03-22 16:30:08 +0900

    modified for supporting play list

--- js/splustar-appspec.js
+++ js/splustar-appspec.js
@@ -5,6 +5,8 @@
 function AppSpec() {
 	this.origin = "set_origin_of_your_app";
 	this.duration = 0;
+	this.playlist = new Array("12_LpNeAxyw", "NYUwUUbfJOM", "_393OZGncrc");
+	this.prevVideoId = "";
 };
 
 AppSpec.prototype.getOrigin = function() {
@@ -17,4 +19,16 @@ AppSpec.prototype.getDuration = function() {
 
 AppSpec.prototype.setDuration = function(duration) {
 	this.duration = duration;
+}
+
+AppSpec.prototype.getPlayList = function() {
+	return this.playlist;
+}
+
+AppSpec.prototype.getPrevVideoId = function() {
+	return this.prevVideoId;
+}
+
+AppSpec.prototype.setPrevVideoId = function(vid) {
+	this.prevVideoId = vid;
 }
